babf753ce1731284ba542e8f56b3127b917a86e8
pesho <peshooomanov@gmail.com>
2025-06-05 15:21:53 +0300

    server: environment variables added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port: string = "8000";
+const port: string = process.env.PORT || "8000";
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));

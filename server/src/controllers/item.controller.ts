e1b35aa250a416178d27836498df91e512d01937
pesho <peshooomanov@gmail.com>
2025-06-05 15:51:57 +0300

    server: getItems endpoint

--- server/src/controllers/item.controller.ts
+++ server/src/controllers/item.controller.ts
@@ -3,7 +3,9 @@ import { IItem } from "../models/interfaces/item.interface";
 
 let items: IItem[] = [];
 
-const getItems = async (req: express.Request, res: express.Response) => {};
+const getItems = async (req: express.Request, res: express.Response) => {
+  res.status(200).json({ items });
+};
 
 const addItem = async (req: express.Request, res: express.Response) => {};
 

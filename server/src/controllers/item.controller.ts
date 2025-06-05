9aa123ff3b6b9bcc3f47d106d9cb2b97ad040b7d
pesho <peshooomanov@gmail.com>
2025-06-05 15:57:43 +0300

    server: addItem endpoint

--- server/src/controllers/item.controller.ts
+++ server/src/controllers/item.controller.ts
@@ -1,12 +1,28 @@
 import express from "express";
 import { IItem } from "../models/interfaces/item.interface";
+import { v4 as uuidv4 } from "uuid";
 
 let items: IItem[] = [];
 
-const getItems = async (req: express.Request, res: express.Response) => {
+const getItems = (req: express.Request, res: express.Response) => {
   res.status(200).json({ items });
 };
 
-const addItem = async (req: express.Request, res: express.Response) => {};
+const addItem = (req: express.Request, res: express.Response) => {
+  const { name } = req.body;
+
+  if (!name || typeof name !== "string") {
+    res.status(400).json({ error: "Invalid name input." });
+    return;
+  }
+
+  const newItem: IItem = {
+    id: uuidv4(),
+    name,
+  };
+
+  items.push(newItem);
+  res.status(201).json(newItem);
+};
 
 export default { getItems, addItem };
